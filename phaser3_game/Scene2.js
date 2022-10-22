882f026d0896ab80470bb5fd7665f9b7ef241fb3
mabrey64 <mabreydn@mail.uc.edu>
2022-10-22 19:12:42 -0400

    Added code after watching video 7 of Phaser 3
    Added code and mages/spritesheets after watching 7th video on Phaser 3.

--- phaser3_game/Scene2.js
+++ phaser3_game/Scene2.js
@@ -11,34 +11,6 @@ class Scene2 extends Phaser.Scene {
         this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
         this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
 
-        this.anims.create({
-            key: "ship1_anim",
-            frames: this.anims.generateFrameNumbers("ship"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "ship2_anim",
-            frames: this.anims.generateFrameNumbers("ship2"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "ship3_anim",
-            frames: this.anims.generateFrameNumbers("ship3"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion"),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: true
-        });
 
         this.ship1.play("ship1_anim");
         this.ship2.play("ship2_anim");
@@ -50,7 +22,36 @@ class Scene2 extends Phaser.Scene {
 
         this.input.on('gameobjectdown', this.destroyShip, this);
 
-        this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
+        this.add.text(20, 20, "Playing game", {
+            font: "25px Arial", 
+            fill: "yellow"
+        });
+
+        this.powerUps = this.physics.add.group();
+
+        var maxObjects = 4;
+        for (var i = 0; i <= maxObjects; i++) {
+            var powerUp = this.physics.add.sprite(16, 16, "power-up");
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+
+            if (Math.random() > 0.5) {
+                powerUp.play("red");
+            } else {
+                powerUp.play("gray");
+            }
+            
+            powerUp.setVelocity(100, 100);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+        } 
+
+        this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
+        this.player.play("thrust");
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+        this.player.setCollideWorldBounds(true);
+
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
@@ -59,12 +60,35 @@ class Scene2 extends Phaser.Scene {
         this.moveShip(this.ship3, 3);
 
         this.background.tilePositionY -= 0.5;
+
+        this.movePlayerManager();
+
+        if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
+            console.log("Fire!");
+        }
     }
 
     moveShip(ship, speed) {
         ship.y +=speed;
         if (ship.y > config.height){
             this.resetShipPos(ship);
+        }
+    }
+
+    movePlayerManager(){
+
+        this.player.setVelocity(0);
+
+        if(this.cursorKeys.left.isDown){
+            this.player.setVelocityX(-gameSettings.playerSpeed);
+        }else if(this.cursorKeys.right.isDown){
+            this.player.setVelocityX(gameSettings.playerSpeed);
+        }
+
+        if(this.cursorKeys.up.isDown){
+            this.player.setVelocityY(-gameSettings.playerSpeed);
+        }else if(this.cursorKeys.down.isDown){
+            this.player.setVelocityY(gameSettings.playerSpeed);
         }
     }
 
--- phaser3_game/game.js
+++ phaser3_game/game.js
@@ -1,12 +1,20 @@
-var config = {
+const gameSettings = {
+    playerSpeed: 200,
+}
+
+const config = {
     width: 256,
     height: 272,
     backgroundColor: 0x000000,
     scene: [Scene1, Scene2],
-    pixelArt: true
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug: false
+        }
+    }
 }
 
 
-window.onload = function(){
-    var game = new Phaser.Game(config);
-}
+const game = new Phaser.Game(config);
